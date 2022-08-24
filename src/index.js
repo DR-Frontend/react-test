8f986d57d63c792a982f34da0a8eba2ac1fbca56
David Romero <dvdrmrrdz@gmail.com>
2022-08-24 18:36:46 -0500

    primeras funciones en index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greetings() {
+  return (
+    <div>
+      <h1>Esto es un componente</h1>
+      <p>Con su respectivo parrafo</p>
+    </div>
+  );
+}
+
+function Suma() {
+  const numero = 30;
+  return <div> La suma es: {10 + numero} </div>;
+}
+
+function Casado() {
+  const married = true;
+
+  return <div> {married ? "Estoy caZado" : "No tengo costilla"} </div>;
+}
+
+function Id(){
+  const user = {
+    name: "Iris",
+    lastName: "Lara"
+  }
+
+  return <div>
+    <h2>Hola me llamo {user.name} {user.lastName}</h2>
+  </div>
+}
+
+function Operacion(){
+  function resta(a,b){
+    return a - b
+  }
+  return <div>
+    <p>{resta(30,10)}</p>
+  </div>
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greetings />
+    <Suma />
+    <Casado />
+    <Id/>
+    <Operacion/>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
