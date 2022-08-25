ec686763d64ff3d42f74d6534999470254588a20
David Romero <dvdrmrrdz@gmail.com>
2022-08-25 01:12:17 -0500

    componente user name

--- src/componentes.js
+++ src/componentes.js
@@ -1,19 +1,12 @@
-export function UserName(){
-   const usuario ={
-      name: "David",
-      lastName: "Romero"
-   }
+export function UserName({name, age, amount, married, points, address}){
    return <div>
-      <p>Mi nombre es: {usuario.name} {usuario.lastName}</p>
+      <h1>{name}</h1>
+      <ul>
+         <li>Edad: {age}</li>
+         <li>Monto: {amount}</li>
+         <li>Edo. Civil: {married ? 'Casado' : 'Soltero'}</li>
+         <li>Puntos: {points}</li>
+         <li>Calle: {address.street}, Ciudad: {address.city}</li>
+      </ul>
    </div>
-}
-
-export function Married(){
-   const married = true
-   return <div> {married ? 'Estoy caZado' : 'No tengo costilla'} </div>
-}
-
-export function Greetings(saludo, nombre='usuario'){
-   console.log(saludo, nombre)
-   return <h1>{saludo} dice {nombre}</h1>
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import {UserName, Married, Greetings} from './componentes'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {UserName} from "./componentes";
+import {Button} from './button'
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <UserName/>
-    <Married />
-    <Greetings saludo='Hola' nombre='Hugo'/>
-    <Greetings saludo='Aló' nombre='Paco'/>
-    <Greetings saludo='Hi' nombre='Luis'/>
-    <Greetings saludo='Buen día' nombre='Laura'/>
-    <Greetings saludo='Aloha'/>
+    <UserName
+      name="Yerik"
+      age={15}
+      amount={3500}
+      married={false}
+      points={[10, 14,35,44,23]}
+      address={{street:"Main street 123", city:"New York"}}
+    />
+    <UserName
+      name="Omar"
+      age={43}
+      amount={6500}
+      married={true}
+      points={[100,30,10]}
+      address={{street:"Evergreen 321", city:"Springfield"}}
+    />
+
+    <Button/>
+
   </>
 );
